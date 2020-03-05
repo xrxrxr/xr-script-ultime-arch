de54702cc6cdce342976abbbf15c4fb4437adfb1
Xavier RE4 <xrthefrench@gmail.com>
2020-03-05 20:25:03 +0100

    better

--- zsh.sh
+++ zsh.sh
@@ -6,4 +6,5 @@ yes | sudo pacman -S curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .vimrc  ~/.vimrc
 sudo chsh -s /usr/bin/zsh
-
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+#ZSH_THEME="powerlevel9k/powerlevel9k"
